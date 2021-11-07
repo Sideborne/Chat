e3c098f35c15be2faa807fe39c69bdbc6770050c
Sideborne <Sideborne@bk.ru>
2021-11-07 21:27:20 +0300

    myProjectisBegin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Message } from "./Components/Message";
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ниже будет компонент с props ввиде текста "My text"</h1>
+      <Message myText='My text' />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/Components/Message.js
@@ -0,0 +1,5 @@
+import './styles.module.css';
+
+export function Message({ myText }) {
+    return <span className='text'>This now here is {myText}</span>
+}
